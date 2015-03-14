6fa2b5ea8e7cad2c4ce5ee35dca9d265f324d845
Jaime Pillora <dev@jpillora.com>
2015-03-15 06:11:41 +1100

    fixed missing var in test package

--- test/chisel_test.go
+++ test/chisel_test.go
@@ -79,6 +79,36 @@ func testTunnel(port string, size int, t *testing.T) {
 
 //============================
 
+func requestFile(port string, size int) (*http.Response, error) {
+	url := "http://127.0.0.1:" + port + "/" + strconv.Itoa(size)
+	// fmt.Println(url)
+	return http.Get(url)
+}
+
+func makeFileServer() *chisel.HTTPServer {
+	bsize := 3 * MB
+	bytes := make([]byte, bsize)
+	//filling huge buffer
+	for i := 0; i < len(bytes); i++ {
+		bytes[i] = byte(i)
+	}
+
+	s := chisel.NewHTTPServer()
+	s.SetKeepAlivesEnabled(false)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rsize, _ := strconv.Atoi(r.URL.Path[1:])
+		for rsize >= bsize {
+			w.Write(bytes)
+			rsize -= bsize
+		}
+		w.Write(bytes[:rsize])
+	})
+	s.GoListenAndServe("0.0.0.0:3000", handler)
+	return s
+}
+
+//============================
+
 //global setup
 func TestMain(m *testing.M) {
 
