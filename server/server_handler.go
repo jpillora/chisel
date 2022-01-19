e38b4fbe728ecff25d6a312205be9be02f422666
bar <foo@example.org>
2022-01-19 20:20:25 +0800

    Respond to /health and /version by request path rather than by the whole url string #327

--- server/server_handler.go
+++ server/server_handler.go
@@ -34,7 +34,7 @@ func (s *Server) handleClientHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	//no proxy defined, provide access to health/version checks
-	switch r.URL.String() {
+	switch r.URL.Path {
 	case "/health":
 		w.Write([]byte("OK\n"))
 		return
